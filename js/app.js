91d8ac13e0a889d26fb2a017b89a6790479b4484
Tyler Huntley <tylershuntley@hotmail.com>
2022-10-26 21:49:03 +0000

    Lab 3 Array

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 
 // console.log('Hello World');
 
+let score = 0;
+
 let visitorName = prompt('Hello! What is your name?');
 // console.log(visitorName);
 alert(`Welcome to my site ${visitorName}! Please answer "Yes" or "No" to the following questions.`);
@@ -10,6 +12,7 @@ let questionOne = prompt('Do I like si-fi?').toUpperCase();
 if (questionOne === 'YES' || questionOne === 'Y') {
   // console.log(questionOne + ' Correct');
   alert('Correct');
+  score++;
 } else if (questionOne === 'NO' || questionOne === 'N') {
   // console.log(questionOne + ' incorrect');
   alert('Incorrect');
@@ -19,6 +22,7 @@ let questionTwo = prompt('Do I like to adventure?').toUpperCase();
 if (questionTwo === 'YES' || questionTwo === 'Y') {
   // console.log(questionTwo + ' Correct');
   alert('Correct');
+  score++;
 } else if (questionTwo === 'NO' || questionTwo === 'N') {
   // console.log(questionTwo + ' incorrect');
   alert('Incorrect');
@@ -28,6 +32,7 @@ let questionThree = prompt('Was I in the Air Force?').toUpperCase();
 if (questionThree === 'YES' || questionThree === 'Y') {
   // console.log(questionThree + ' Correct');
   alert('Correct');
+  score++;
 } else if (questionThree === 'NO' || questionThree === 'N') {
   // console.log(questionThree + ' incorrect');
   alert('Incorrect');
@@ -37,6 +42,7 @@ let questionFour = prompt('Do I have any pets?').toUpperCase();
 if (questionFour === 'NO' || questionFour === 'N') {
   // console.log(questionFour + ' Correct');
   alert('Correct');
+  score++;
 } else if (questionFour === 'YES' || questionFour === 'Y') {
   // console.log(questionFour + ' incorrect');
   alert('Incorrect');
@@ -46,6 +52,7 @@ let questionFive = prompt('Do I like food?').toUpperCase();
 if (questionFive === 'YES' || questionFive === 'Y') {
   // console.log(questionFive + ' Correct');
   alert('Correct');
+  score++;
 } else if (questionFive === 'NO' || questionFive === 'N') {
   // console.log(questionFive + ' incorrect');
   alert('Incorrect');
@@ -57,6 +64,7 @@ while (guesses > 0) {
   let questionSix = prompt('What is my favorite number between 1-20?');
   if (questionSix === favNum) {
     alert('Good guess!');
+    score++;
     break;
   }
   else if (questionSix > favNum) {
@@ -68,22 +76,27 @@ while (guesses > 0) {
   guesses--;
 }
 
-// let heatSource = ['Electric', 'Gas', 'Hydronic', 'Geothermal'];
-// let guessesTwo = 6;
-// let answer = false;
-// whlie(guessesTwo > 0) {
-//   let questionSeven = prompt('What are the most common home heat sources?')
-//   for (let i = 0; i < heatSource.length; i++) {
-//     if (heatSource[i] === questionSeven) {
-//       alert(`Correct! ${heatSource[i]} is a common heat source!`);
-//       answer = true;
-//     }
-//     else (alert(`${questionSeven} is not a common heat source. ${guessesTwo} guesses remaining.`));
-//   }
-//   if (answer) {
-//     break;
-//   }
-//   guessesTwo--;
-// }
+let heatSource = ['electric', 'gas', 'hydronic', 'geothermal'];
+let guessesTwo = 6;
+let isCorrect = false;
+while (guessesTwo > 0) {
+  let questionSeven = prompt('What are the most common home heat sources?').toLowerCase();
+  for (let i = 0; i < heatSource.length; i++) {
+    if (heatSource[i] === questionSeven) {
+      isCorrect = true;
+    }
+  }
+  if (isCorrect) {
+    alert(`Correct! ${questionSeven} is a common heat source!`);
+    score++;
+    break;
+  } else {
+    alert(`${questionSeven} is not a common heat source. ${guessesTwo} guesses remaining.`);
+  }
+  guessesTwo--;
+}
+alert(`The possible answers were: ${heatSource[0]}, ${heatSource[1]}, ${heatSource[2]}, ${heatSource[3]}.`);
+
+alert(`Your final score is ${score}.`);
 
 alert(`Thanks for playing, ${visitorName}. It's nice of you to stop by!`);

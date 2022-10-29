1010eba4d9fe999a749f9d42c3bb7ecc571e4170
Tyler Huntley <tylershuntley@hotmail.com>
2022-10-29 00:19:34 +0000

    remove commented out logs and code

--- js/app.js
+++ js/app.js
@@ -1,37 +1,31 @@
 'use strict';
 
-// console.log('Hello World');
-
-// Global variables
-
 let score = 0;
 
-// Function Declarations/Definitions
-
 function sifi() {
-  let questionOne = prompt('Do I like si-fi?').toUpperCase();
-  if (questionOne === 'YES' || questionOne === 'Y') {
-    // console.log(questionOne + ' Correct');
-    alert('Correct');
-    score++;
-  } else if (questionOne === 'NO' || questionOne === 'N') {
-    // console.log(questionOne + ' incorrect');
-    alert('Incorrect');
+  let validAnswer = false;
+  while (!validAnswer) {
+    let questionOne = prompt('Do I like si-fi?').toUpperCase();
+    if (questionOne === 'YES' || questionOne === 'Y') {
+      alert('Correct');
+      validAnswer = true;
+      score++;
+    } else if (questionOne === 'NO' || questionOne === 'N') {
+      alert('Incorrect');
+      validAnswer = true;
+    }
   }
 }
 
 function adventure() {
-  // added boolean to continue prompting until we get a valid answer
   let validAnswer = false;
   while (!validAnswer) {
     let questionTwo = prompt('Do I like to adventure?').toUpperCase();
     if (questionTwo === 'YES' || questionTwo === 'Y') {
-      // console.log(questionTwo + ' Correct');
       alert('Correct');
       validAnswer = true;
       score++;
     } else if (questionTwo === 'NO' || questionTwo === 'N') {
-      // console.log(questionTwo + ' incorrect');
       alert('Incorrect');
       validAnswer = true;
     }
@@ -39,38 +33,47 @@ function adventure() {
 }
 
 function military() {
-  let questionThree = prompt('Was I in the Air Force?').toUpperCase();
-  if (questionThree === 'YES' || questionThree === 'Y') {
-    // console.log(questionThree + ' Correct');
-    alert('Correct');
-    score++;
-  } else if (questionThree === 'NO' || questionThree === 'N') {
-    // console.log(questionThree + ' incorrect');
-    alert('Incorrect');
+  let validAnswer = false;
+  while (!validAnswer) {
+    let questionThree = prompt('Was I in the Air Force?').toUpperCase();
+    if (questionThree === 'YES' || questionThree === 'Y') {
+      alert('Correct');
+      validAnswer = true;
+      score++;
+    } else if (questionThree === 'NO' || questionThree === 'N') {
+      alert('Incorrect');
+      validAnswer = true;
+    }
   }
 }
 
 function pets() {
-  let questionFour = prompt('Do I have any pets?').toUpperCase();
-  if (questionFour === 'NO' || questionFour === 'N') {
-    // console.log(questionFour + ' Correct');
-    alert('Correct');
-    score++;
-  } else if (questionFour === 'YES' || questionFour === 'Y') {
-    // console.log(questionFour + ' incorrect');
-    alert('Incorrect');
+  let validAnswer = false;
+  while (!validAnswer) {
+    let questionFour = prompt('Do I have any pets?').toUpperCase();
+    if (questionFour === 'NO' || questionFour === 'N') {
+      alert('Correct');
+      validAnswer = true;
+      score++;
+    } else if (questionFour === 'YES' || questionFour === 'Y') {
+      alert('Incorrect');
+      validAnswer = true;
+    }
   }
 }
 
 function food() {
-  let questionFive = prompt('Do I like food?').toUpperCase();
-  if (questionFive === 'YES' || questionFive === 'Y') {
-    // console.log(questionFive + ' Correct');
-    alert('Correct');
-    score++;
-  } else if (questionFive === 'NO' || questionFive === 'N') {
-    // console.log(questionFive + ' incorrect');
-    alert('Incorrect');
+  let validAnswer = false;
+  while (!validAnswer) {
+    let questionFive = prompt('Do I like food?').toUpperCase();
+    if (questionFive === 'YES' || questionFive === 'Y') {
+      alert('Correct');
+      validAnswer = true;
+      score++;
+    } else if (questionFive === 'NO' || questionFive === 'N') {
+      alert('Incorrect');
+      validAnswer = true;
+    }
   }
 }
 
@@ -118,10 +121,8 @@ function heat() {
   alert(`The possible answers were: ${heatSource[0]}, ${heatSource[1]}, ${heatSource[2]}, ${heatSource[3]}.`);
 }
 
-// Executable Code
 
 let visitorName = prompt('Hello! What is your name?');
-// console.log(visitorName);
 alert(`Welcome to my site ${visitorName}! Please answer "Yes" or "No" to the following questions.`);
 
 sifi();
